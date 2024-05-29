a031b375ef75bb8f78996c6807e78ffddb3268c8
Game Thanapon <134900556+TopcloudThanapon@users.noreply.github.com>
2024-05-29 13:05:54 +0700

    update seo

--- app.vue
+++ app.vue
@@ -7,11 +7,27 @@
 </template>
 
 <script setup lang="ts">
+useSeoMeta({
+  title: "Veather | Weather App",
+  ogTitle: "Veather | Weather App",
+  description: "A simple weather app to check the weather in your location.",
+  ogDescription: "A simple weather app to check the weather in your location.",
+  ogImage: "https://weather.moonman.work/favicon.svg",
+  twitterCard: "summary_large_image",
+});
+
 const darkModeActive = useLocalStorage("darkMode", true);
 useHead({
   htmlAttrs: {
     class: darkModeActive.value ? "dark" : "",
   },
+  link: [
+    {
+      rel: "icon",
+      type: "image/svg+xml",
+      href: "/favicon.svg",
+    },
+  ],
 });
 
 export type AppProvide = {
